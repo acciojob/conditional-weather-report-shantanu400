2d0a53aadce8776ee621aff46f0d479d8522ed85
shantanu400 <ashunayan400@gmail.com>
2024-07-10 18:23:14 +0530

    1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,36 @@
 
-import React from "react";
+import React, { useState,useEffect } from "react";
 import './../styles/App.css';
+import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
+  const [weatherData,setWeatherData]=useState([
+    {temperature: 25, conditions: "Sunny" }
+  ])
+
+  useEffect(() => {
+    let pas = "931d9a0688d73e3af0451ed87dcfe233";
+    let fetc = `https://api.openweathermap.org/data/2.5/weather?q=${"patna"}&units=metric&appid=${pas}`;
+    fetch(fetc)
+      .then((data) => {
+        return data.json();
+      })
+      .then((data1) => {
+        console.log(data1);
+        setWeatherData([
+          { temperature: data1.main.temp, conditions: data1.weather[0].description },
+        ]);
+      })
+      .catch((error) => {
+        alert("sorry data not found");
+      });
+  }, []);
+
+  
   return (
     <div>
-        {/* Do not remove the main div */}
+      
+         <WeatherDisplay weatherData={weatherData[0]}/>
     </div>
   )
 }
---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function WeatherDisplay(props){
+    const {temperature,conditions} = props.weatherData;
+    const style = {
+        color: temperature >= 20 ? 'red' : 'blue', 
+        height: '100px', 
+        width: '60px', 
+        display: 'inline'
+    };
+    return (
+        <div style={style}>
+        <p>{temperature}</p>
+        <p>{conditions}</p>
+    </div>
+    )
+}
